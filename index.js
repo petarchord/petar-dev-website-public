e53c4cc22d9ea56087d224b2f9dc77c4323f2f6c
Petar Jovanovic <petarjovanovic@SYM0739.local>
2022-11-17 15:12:21 +0100

    new updates

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let myHover3D = new Hover3D(".profile-img-wrapper");
 
 const hamburgerButton = document.getElementById("hamburger-button");
-console.log("hambButton:", hamburgerButton);
 const header = document.getElementsByTagName("header")[0];
-console.log("header:", header);
 const mobileNav = document.getElementsByClassName("mobile-nav")[0];
-console.log("mobile-nav:", mobileNav);
+const form = document.getElementsByTagName("form")[0];
+const inputName = form[0];
+const inputEmail = form[1];
+const inputPhone = form[2];
+const inputSubject = form[3];
+const inputMessage = form[4];
+const errorName = document.getElementsByClassName("error-name")[0];
+const errorEmail = document.getElementsByClassName("error-email")[0];
+const errorMessage = document.getElementsByClassName("error-area")[0];
+const loader = document.getElementsByClassName("loader")[0];
+const emailModal = document.getElementsByClassName("email-modal")[0];
+const closeModalButton = document.getElementById("close-modal-button");
+const emailModalFailure = document.getElementsByClassName(
+  "email-modal-failure"
+)[0];
+const closeModalFailure = document.getElementById("close-modal-failure");
 
 hamburgerButton.addEventListener("click", function () {
   this.classList.toggle("open");
@@ -40,8 +53,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
       document.getElementsByClassName("animated-element");
     const windowOffsetTop = window.innerHeight + window.scrollY;
 
-    console.log("animatedElements:", animatedElements);
-
     Array.prototype.forEach.call(animatedElements, function (element, index) {
       const animatedElementOffset = element.offsetTop;
 
@@ -57,4 +68,73 @@ function addClass(element, className) {
   if (arrayClasses.indexOf(className) === -1) {
     element.className += " " + className;
   }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (!validateForm()) return;
+  else {
+    loader.style.display = "block";
+    Email.send({
+      SecureToken: "62e82d2a-60f9-479a-9d91-3ee4219a6b61",
+      To: "petar@petardev.com",
+      From: inputEmail.value,
+      Subject: inputSubject.value ? inputSubject.value : "",
+      Body: inputMessage.value,
+    })
+      .then((message) => {
+        emailModal.style.display = "block";
+      })
+      .catch((error) => {
+        emailModalFailure.style.display = "block";
+      })
+      .finally(() => {
+        loader.style.display = "none";
+      });
+  }
+});
+
+closeModalButton.addEventListener("click", function () {
+  emailModal.style.display = "none";
+  form.reset();
+});
+
+closeModalFailure.addEventListener("click", function () {
+  emailModalFailure.style.display = "none";
+  form.reset();
+});
+
+function validateRequiredField(input, errorMessage) {
+  if (!input.value) {
+    errorMessage.innerHTML = "This field is required.";
+    errorMessage.style.display = "block";
+    input.style.border = "solid .05rem red";
+    return false;
+  } else {
+    input.style.border = "solid 0.05rem aliceblue";
+    errorMessage.style.display = "none";
+    return true;
+  }
+}
+
+function validateEmailField(input, errorMessage) {
+  let regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!input.value.match(regex)) {
+    errorMessage.innerHTML = "Please enter a valid email address.";
+    errorMessage.style.display = "block";
+    input.style.border = "solid .05rem red";
+    return false;
+  } else {
+    input.style.border = "solid 0.05rem aliceblue";
+    errorMessage.style.display = "none";
+    return true;
+  }
+}
+
+function validateForm() {
+  let isNameValid = validateRequiredField(inputName, errorName);
+  let isMessageValid = validateRequiredField(inputMessage, errorMessage);
+  let isEmailValid = validateEmailField(inputEmail, errorEmail);
+  return isNameValid && isMessageValid && isEmailValid;
 }
